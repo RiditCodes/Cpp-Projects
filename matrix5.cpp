34ba8c6cb8250f21746ab1e14cfaf1d6e9f796b4
RiditCodes <90456597+RiditCodes@users.noreply.github.com>
2025-03-01 11:37:18 +0530

    refactor: remove unnecessary transpose logic and add example output in comments

--- matrix5.cpp
+++ matrix5.cpp
@@ -20,12 +20,6 @@ int main(){
         cout<<endl;
     }
 
-    for (row = 0;row < 2; row++){
-        for (column = 0; column < 3; column++){
-            arr2[row][column] = arr1[column][row];
-        }
-    }
-
     cout<<"The transpose of the matrix is..."<<endl;
     for (row = 0; row < 3; row++){
         for (column = 0; column < 2; column++){
@@ -39,3 +33,14 @@ int main(){
         cout<<endl;
     }
 }
+
+/*
+
+1 2 3
+4 5 6
+
+1 4
+2 5
+3 6
+
+*/
